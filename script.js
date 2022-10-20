5348cc9b28d9b01b2a180b19cea0735806e0ebbf
Hank Liu <hank33liu@gmail.com>
2022-10-20 01:06:46 -0700

    Add HTML/CSS skeleton for library and cards-grid

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+//Book constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read
+}
+
+function addBookToLibrary() {
+    
+}
