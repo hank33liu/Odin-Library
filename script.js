606a3c7ea6a62c8edf6b98f1c41fb051b932891b
Hank Liu <hank33liu@gmail.com>
2022-10-21 23:54:23 -0700

    Add basic JavaScript. Adds inheritance testing code with pseudoclasses

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 let myLibrary = [];
 
 //Book constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read
+class Book {
+    constructor(
+        title = 'Unknown',
+        author = 'Unknown',
+        pages = '0',
+        isRead = False
+    ) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read
+    }
 }
 
-function addBookToLibrary() {
-    
+class Library {
+    constructor() {
+        this.books = [];
+    }
+
+    addBook() {
+        
+    }
 }
+
+// TESTING LINE DO NOT CROSS
+
+// class Student {
+//     constructor(name) {
+//         this.name = name;
+//     }
+
+//     sayName() {
+//         return this.name;
+//     }
+// }
+
+
+// class EigthGrader extends Student {
+//     constructor(name) {
+//         super();
+//         this.name = name;
+//         this.grade = 8;
+//     }
+// }
+
+// // EigthGrader.prototype = Object.create(Student.prototype)
+
+// const carl = new EigthGrader("carl")
+// console.log(carl.sayName())
