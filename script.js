5ac95624c5645e4dd2da0907f69494e4a1fe7d94
Hank Liu <hank33liu@gmail.com>
2022-10-23 23:56:14 -0700

    Add modal and grey overlay. Add function to Add book button

--- script.js
+++ script.js
@@ -21,9 +21,48 @@ class Library {
     }
 
     addBook() {
-        
+        pass
     }
 }
+
+//Submit form
+const getBookFromInput = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const isRead = document.getElementById('isRead').checked
+    return new Book(title, author, pages, isRead)
+}
+
+// const createBookCard = (book)
+
+
+//Modal
+const addBook = document.getElementById("add-book"); //get button that opens addBookModal
+
+const overlay = document.getElementById("modal-overlay"); //get modal overlay
+
+const addBookModal = document.getElementById("addBookModal"); //get addBookModal
+
+const showAddBookModal = () => {
+    overlay.style.display = "block";
+    addBookModal.style.display = "block";
+}
+
+const hideAddBookModal = () => {
+    overlay.style.display = "none";
+    addBookModal.style.display = "none";
+}
+
+addBook.addEventListener("click", showAddBookModal) //Show overlay and addBookModal when Add book button is clicked
+
+overlay.addEventListener("click", hideAddBookModal) //Hide overlay and addBookModal when overlay is clicked
+
+window.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+        console.log("Escaped");
+        hideAddBookModal()};
+})
 
 // TESTING LINE DO NOT CROSS
 
